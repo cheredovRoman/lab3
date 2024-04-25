1557f1c4c5b9309c632353910b73fe417b33eea9
79061 <romacheredov@gmail.com>
2024-04-25 06:40:20 +0300

    Refactoring generate_enemy_ships() trivial implementation for pass the test

--- logic.py
+++ logic.py
@@ -1,6 +1,6 @@
 import random
-def generate_enemy_ships(size):
+def generate_enemy_ships(size, fill):
     if size == 0:
         return 0
-    enemy_ships = [[0 for i in range(size)] for i in range(size)]
+    enemy_ships = [[fill for i in range(size)] for i in range(size)]
     return enemy_ships
