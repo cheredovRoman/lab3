7640ce4a82cf53b646bc3ce7b3e0d6b62837f437
79061 <romacheredov@gmail.com>
2024-04-25 06:37:45 +0300

    Added test for generate_enemy_ships(enemy_ships)

--- TestGame.py
+++ TestGame.py
@@ -3,9 +3,14 @@ from logic import *
 
 class TestGame(unittest.TestCase):
 
+    # def test_game_class_exists(self):
+    #     self.assertTrue('logic' in globals())
+    def test_generate_enemy_ships(self):
+        self.assertEqual(generate_enemy_ships(), 0)
 
-    #def test_generate_enemy_ships(self):
-       # self.assertEqual(generate_enemy_ships(), 0)
+    def test_generate_enemy_ships_size_5(self):
+        enemy_ships = [[0 for i in range(5)] for i in range(5)]
+        self.assertEqual(generate_enemy_ships(), enemy_ships)
 
 if __name__ == '__main__':
     unittest.main()
