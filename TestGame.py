f1fa882748f2e152ad776b04616d2e998de42197
79061 <romacheredov@gmail.com>
2024-04-25 06:31:42 +0300

    A window form has been created, a playing field has been drawn, and mouse events have been read

--- TestGame.py
+++ TestGame.py
@@ -1,10 +1,12 @@
 import unittest
-from Game import Game
+from game import *
 
 class TestGame(unittest.TestCase):
 
     def test_game_class_exists(self):
         self.assertTrue('Game' in globals())
+    def test_draw_table(self):
+        self.assertEqual()
 
 if __name__ == '__main__':
     unittest.main()
