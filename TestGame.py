e8576dbd65de34386c682e2e2930f5ae51d27da4
79061 <romacheredov@gmail.com>
2024-04-25 06:38:41 +0300

    Refactoring generate_enemy_ships() trivial implementation for pass the test

--- TestGame.py
+++ TestGame.py
@@ -6,11 +6,11 @@ class TestGame(unittest.TestCase):
     # def test_game_class_exists(self):
     #     self.assertTrue('logic' in globals())
     def test_generate_enemy_ships(self):
-        self.assertEqual(generate_enemy_ships(), 0)
+        self.assertEqual(generate_enemy_ships(0), 0)
 
     def test_generate_enemy_ships_size_5(self):
         enemy_ships = [[0 for i in range(5)] for i in range(5)]
-        self.assertEqual(generate_enemy_ships(), enemy_ships)
+        self.assertEqual(generate_enemy_ships(5), enemy_ships)
 
 if __name__ == '__main__':
     unittest.main()
--- logic.py
+++ logic.py
@@ -1,4 +1,6 @@
 import random
-def generate_enemy_ships():
-    # Позже реализовать метод генерации кораблей
-    return 0
+def generate_enemy_ships(size):
+    if size == 0:
+        return 0
+    enemy_ships = [[0 for i in range(size)] for i in range(size)]
+    return enemy_ships
